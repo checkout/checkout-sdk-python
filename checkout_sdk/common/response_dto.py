2495a49ee562432b56d95f4c715c456f367f6425
Riaz Bordie <riaz.bordie@checkout.com>
2018-12-09 23:07:00 +0000

    - Reverted setup.py

--- checkout_sdk/common/response_dto.py
+++ checkout_sdk/common/response_dto.py
@@ -5,17 +5,16 @@ class ResponseDTO:
 
     # add class attributes to enable access via dot notation
     def _create_attributes(self):
-        for key in self._kvp:
-            val = self._kvp[key]
-            if isinstance(val, dict):
-                setattr(self, key, ResponseDTO(val))
-            elif isinstance(val, list):
+        for (k, v) in self._kvp.items():
+            if isinstance(v, dict):
+                setattr(self, k, ResponseDTO(v))
+            elif isinstance(v, list):
                 dto_list = []
-                for item in val:
+                for item in v:
                     dto_list.append(ResponseDTO(item))
-                setattr(self, key, dto_list)
+                setattr(self, k, dto_list)
             else:
-                setattr(self, key, val)
+                setattr(self, k, v)
 
     @property
     def _attr_count(self):
