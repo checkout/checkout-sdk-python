f059fc77ca7e549404565ea327801f55cb423a66
Michael <michael.jaszczuk@gmail.com>
2019-02-26 15:52:13 +0100

    Python 3.4 support

--- checkout_sdk/cards/cards_client.py
+++ checkout_sdk/cards/cards_client.py
@@ -10,19 +10,21 @@ class CardsClient(ApiClient):
     def get_cards(self, customer_id):
         self._log_info('Get cards for {}'.format(customer_id))
         http_response = self._send_http_request(
-            f'customers/{customer_id}/cards/', sdk.HttpMethod.GET)
+            'customers/{}/cards/'.format(customer_id), sdk.HttpMethod.GET)
         return CardListResponse(http_response)
 
     def get_card(self, customer_id, card_id):
         self._log_info('Get card {}{}'.format(customer_id, card_id))
         http_response = self._send_http_request(
-            f'customers/{customer_id}/cards/{card_id}', sdk.HttpMethod.GET)
+            'customers/{}/cards/{}'.format(customer_id, card_id),
+            sdk.HttpMethod.GET
+        )
         return CardResponse(http_response)
 
     def remove_card(self, customer_id, card_id):
         self._log_info('Remove card {}{}'.format(customer_id, card_id))
         http_response = self._send_http_request(
-            f'customers/{customer_id}/cards/{card_id}',
+            'customers/{}/cards/{}'.format(customer_id, card_id),
             HttpMethod.DELETE
         )
         return ApiResponse(http_response)
