ed55d1a6b3c505711c1c6de2db397252a64d66d1
Armando Ibarra <aibarrakerzon@gmail.com>
2022-04-07 22:23:27 -0500

    Add support for Get action invocations Workflows (CS2)

--- checkout_sdk/workflows/workflows_client.py
+++ checkout_sdk/workflows/workflows_client.py
@@ -24,29 +24,38 @@ class WorkflowsClient(Client):
                          authorization_type=AuthorizationType.SECRET_KEY_OR_OAUTH)
 
     def create_workflow(self, create_workflow_request: CreateWorkflowRequest):
-        return self._api_client.post(self.__WORKFLOWS_PATH, self._sdk_authorization(), create_workflow_request)
+        return self._api_client.post(self.__WORKFLOWS_PATH,
+                                     self._sdk_authorization(),
+                                     create_workflow_request)
 
     def get_workflows(self):
-        return self._api_client.get(self.__WORKFLOWS_PATH, self._sdk_authorization())
+        return self._api_client.get(self.__WORKFLOWS_PATH,
+                                    self._sdk_authorization())
 
     def get_workflow(self, workflow_id: str):
-        return self._api_client.get(self.build_path(self.__WORKFLOWS_PATH, workflow_id), self._sdk_authorization())
+        return self._api_client.get(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
+                                    self._sdk_authorization())
 
     def update_workflow(self, workflow_id: str, update_workflow_request: UpdateWorkflowRequest):
-        return self._api_client.patch(self.build_path(self.__WORKFLOWS_PATH, workflow_id), self._sdk_authorization(),
+        return self._api_client.patch(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
+                                      self._sdk_authorization(),
                                       update_workflow_request)
 
     def remove_workflow(self, workflow_id: str):
-        self._api_client.delete(self.build_path(self.__WORKFLOWS_PATH, workflow_id), self._sdk_authorization())
+        return self._api_client.delete(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
+                                       self._sdk_authorization())
 
     def update_workflow_action(self, workflow_id: str, action_id: str, workflow_action_request: WorkflowActionRequest):
-        self._api_client.put(self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__ACTIONS_PATH, action_id),
-                             self._sdk_authorization(), workflow_action_request)
+        return self._api_client.put(self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__ACTIONS_PATH, action_id),
+                                    self._sdk_authorization(),
+                                    workflow_action_request)
 
     def update_workflow_condition(self, workflow_id: str, condition_id: str,
                                   workflow_condition_request: WorkflowConditionRequest):
-        self._api_client.put(self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__CONDITIONS_PATH, condition_id),
-                             self._sdk_authorization(), workflow_condition_request)
+        return self._api_client.put(
+            self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__CONDITIONS_PATH, condition_id),
+            self._sdk_authorization(),
+            workflow_condition_request)
 
     def get_event_types(self):
         return self._api_client.get(self.build_path(self.__WORKFLOWS_PATH, self.__EVENT_TYPES_PATH),
@@ -59,27 +68,38 @@ class WorkflowsClient(Client):
 
     def get_event(self, event_id: str):
         return self._api_client.get(
-            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id), self._sdk_authorization())
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id),
+            self._sdk_authorization())
 
     def reflow_by_event(self, event_id: str):
-        self._api_client.post(self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__REFLOW_PATH),
-                              self._sdk_authorization())
+        return self._api_client.post(
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__REFLOW_PATH),
+            self._sdk_authorization())
 
     def reflow_by_subject(self, subject_id: str):
-        self._api_client.post(
+        return self._api_client.post(
             self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
-                            self.__REFLOW_PATH), self._sdk_authorization())
+                            self.__REFLOW_PATH),
+            self._sdk_authorization())
 
     def reflow_by_event_and_workflow(self, event_id: str, workflow_id: str):
         return self._api_client.post(
             self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__WORKFLOW_PATH, workflow_id,
-                            self.__REFLOW_PATH), self._sdk_authorization())
+                            self.__REFLOW_PATH),
+            self._sdk_authorization())
 
     def reflow_by_subject_and_workflow(self, subject_id: str, workflow_id: str):
         return self._api_client.post(
             self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
-                            self.__WORKFLOW_PATH, workflow_id, self.__REFLOW_PATH), self._sdk_authorization())
+                            self.__WORKFLOW_PATH, workflow_id, self.__REFLOW_PATH),
+            self._sdk_authorization())
 
     def reflow(self, reflow_request: ReflowRequest):
-        self._api_client.post(self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__REFLOW_PATH),
-                              self._sdk_authorization(), reflow_request)
+        return self._api_client.post(self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__REFLOW_PATH),
+                                     self._sdk_authorization(),
+                                     reflow_request)
+
+    def get_action_invocations(self, event_id: str, action_id: str):
+        return self._api_client.get(
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__ACTIONS_PATH, action_id),
+            self._sdk_authorization())
