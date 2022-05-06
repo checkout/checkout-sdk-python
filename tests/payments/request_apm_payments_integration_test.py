a66d8e014f7aa4466a32138fd3366d6562e8272c
Armando Ibarra <aibarrakerzon@gmail.com>
2022-05-05 21:38:56 -0500

    Review Payments Schema (CS1/CS2)
    - Added missing APM
    - Refactor Payer to be a common object
    - Added missing processing settings to `processing` property
    - Refactor some tests

--- tests/payments/request_apm_payments_integration_test.py
+++ tests/payments/request_apm_payments_integration_test.py
@@ -4,22 +4,67 @@ import pytest
 
 from checkout_sdk.common.enums import Country, Currency
 from checkout_sdk.payments.payments import PaymentRequest
-from checkout_sdk.payments.payments_apm import RequestBalotoSource, BalotoPayer, Payer, IntegrationType, \
+from checkout_sdk.payments.payments_apm import RequestBalotoSource, IntegrationType, \
     RequestBoletoSource, RequestFawrySource, RequestGiropaySource, RequestIdealSource, RequestOxxoSource, \
-    RequestPagoFacilSource, RequestRapiPagoSource, RequestSofortSource, FawryProduct
-from tests.checkout_test_utils import assert_response, random_email, NAME, retriable
+    RequestPagoFacilSource, RequestRapiPagoSource, RequestSofortSource, FawryProduct, RequestAlipaySource, \
+    RequestBenefitPaySource, RequestEpsSource, RequestKnetSource, RequestP24Source, RequestPayPalSource, \
+    RequestPoliSource, RequestBancontactSource, RequestQPaySource, RequestMultiBancoSource
+from tests.checkout_test_utils import assert_response, retriable, get_payer
+
+
+@pytest.mark.skip(reason='not available')
+def test_should_make_ali_pay_payment(default_api):
+    request_source = RequestAlipaySource()
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.USD
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+
+    assert_response(payment_response, 'id')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+@pytest.mark.skip(reason='not available')
+def test_should_make_benefit_pay_payment(default_api):
+    request_source = RequestBenefitPaySource()
+    request_source.integration_type = 'mobile'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.USD
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+
+    assert_response(payment_response, 'id')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
 
 
 @pytest.mark.skip(reason='not available')
 def test_should_request_baloto_payment(default_api):
-    payer = BalotoPayer()
-    payer.email = random_email()
-    payer.name = NAME
-
     request_source = RequestBalotoSource()
     request_source.country = Country.CO
     request_source.description = 'simulate Via Baloto Demo Payment'
-    request_source.payer = payer
+    request_source.payer = get_payer()
 
     payment_request = PaymentRequest()
     payment_request.source = request_source
@@ -43,21 +88,40 @@ def test_should_request_baloto_payment(default_api):
 @pytest.mark.skip(reason='not available')
 @pytest.mark.parametrize('integration_type', [IntegrationType.REDIRECT, IntegrationType.DIRECT])
 def test_should_request_boleto_payment(default_api, integration_type: IntegrationType):
-    payer = Payer()
-    payer.email = random_email()
-    payer.name = NAME
-    payer.document = '53033315550'
-
     request_source = RequestBoletoSource()
     request_source.country = Country.BR
     request_source.description = 'boleto payment'
-    request_source.payer = payer
+    request_source.payer = get_payer()
     request_source.integration_type = integration_type
 
     payment_request = PaymentRequest()
     payment_request.source = request_source
     payment_request.amount = 100
     payment_request.currency = Currency.BRL
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_eps_payment(default_api):
+    request_source = RequestEpsSource()
+    request_source.purpose = 'Mens black t-shirt L'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.EUR
 
     payment_response = retriable(callback=default_api.payments.request_payment,
                                  payment_request=payment_request)
@@ -161,14 +225,10 @@ def test_should_request_ideal_payment(default_api):
 
 @pytest.mark.skip(reason='not available')
 def test_should_request_oxxo_payment(default_api):
-    payer = Payer()
-    payer.email = random_email()
-    payer.name = NAME
-
     request_source = RequestOxxoSource()
     request_source.country = Country.MX
     request_source.description = 'description'
-    request_source.payer = payer
+    request_source.payer = get_payer()
 
     payment_request = PaymentRequest()
     payment_request.source = request_source
@@ -192,14 +252,10 @@ def test_should_request_oxxo_payment(default_api):
 
 @pytest.mark.skip(reason='not available')
 def test_should_request_pagofacil_payment(default_api):
-    payer = Payer()
-    payer.email = random_email()
-    payer.name = NAME
-
     request_source = RequestPagoFacilSource()
     request_source.country = Country.AR
     request_source.description = 'description'
-    request_source.payer = payer
+    request_source.payer = get_payer()
 
     payment_request = PaymentRequest()
     payment_request.source = request_source
@@ -223,14 +279,10 @@ def test_should_request_pagofacil_payment(default_api):
 
 @pytest.mark.skip(reason='not available')
 def test_should_request_rapipago_payment(default_api):
-    payer = Payer()
-    payer.email = random_email()
-    payer.name = NAME
-
     request_source = RequestRapiPagoSource()
     request_source.country = Country.AR
     request_source.description = 'description'
-    request_source.payer = payer
+    request_source.payer = get_payer()
 
     payment_request = PaymentRequest()
     payment_request.source = request_source
@@ -252,12 +304,196 @@ def test_should_request_rapipago_payment(default_api):
                     '_links')
 
 
-@pytest.mark.skip(reason='not available')
 def test_should_request_sofort_payment(default_api):
     payment_request = PaymentRequest()
     payment_request.source = RequestSofortSource()
-    payment_request.amount = 100000
-    payment_request.currency = Currency.ARS
+    payment_request.amount = 10000
+    payment_request.currency = Currency.EUR
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_knet_payment(default_api):
+    request_source = RequestKnetSource()
+    request_source.language = "en"
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.KWD
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_przelewy24_payment(default_api):
+    request_source = RequestP24Source()
+    request_source.payment_country = Country.PL
+    request_source.account_holder_name = 'Bruce Wayne'
+    request_source.account_holder_email = 'ruce@wayne-enterprises.com'
+    request_source.billing_descriptor = 'P24 Demo Payment'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.PLN
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_pay_pal_payment(default_api):
+    request_source = RequestPayPalSource()
+    request_source.invoice_number = 'CKO00001'
+    request_source.logo_url = 'https://www.example.com/logo.jpg'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.EUR
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_poli_payment(default_api):
+    payment_request = PaymentRequest()
+    payment_request.source = RequestPoliSource()
+    payment_request.amount = 100
+    payment_request.currency = Currency.AUD
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_bancontact_payment(default_api):
+    request_source = RequestBancontactSource()
+    request_source.payment_country = Country.BE
+    request_source.account_holder_name = 'Bruce Wayne'
+    request_source.account_holder_email = 'ruce@wayne-enterprises.com'
+    request_source.billing_descriptor = 'bancontact Demo Payment'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.EUR
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+def test_should_make_qpay_payment(default_api):
+    request_source = RequestQPaySource()
+    request_source.description = 'QPay Demo Payment'
+    request_source.language = 'en'
+    request_source.quantity = 1
+    request_source.national_id = '070AYY010BU234M'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.QAR
+    payment_request.capture = True
+
+    payment_response = retriable(callback=default_api.payments.request_payment,
+                                 payment_request=payment_request)
+    assert_response(payment_response,
+                    'id',
+                    '_links')
+
+    payment_details = retriable(callback=default_api.payments.get_payment_details,
+                                payment_id=payment_response.id)
+    assert_response(payment_details,
+                    'id',
+                    'source',
+                    'amount',
+                    '_links')
+
+
+@pytest.mark.skip(reason='not available')
+def test_should_make_multi_banco_payment(default_api):
+    request_source = RequestMultiBancoSource()
+    request_source.payment_country = Country.PT
+    request_source.account_holder_name = 'Bruce Wayne'
+    request_source.billing_descriptor = 'Multibanco Demo Payment'
+
+    payment_request = PaymentRequest()
+    payment_request.source = request_source
+    payment_request.amount = 100
+    payment_request.currency = Currency.QAR
     payment_request.capture = True
 
     payment_response = retriable(callback=default_api.payments.request_payment,
