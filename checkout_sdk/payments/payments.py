a0c668da3b95262b7a43d362cf4a2d0840c2af32
Raul Estrela <rsaestrela@gmail.com>
2022-05-17 12:45:34 +0200

    Adds missing payment exemptions

--- checkout_sdk/payments/payments.py
+++ checkout_sdk/payments/payments.py
@@ -12,6 +12,11 @@ class Exemption(str, Enum):
     SECURE_CORPORATE_PAYMENT = 'secure_corporate_payment'
     TRUSTED_LISTING = 'trusted_listing'
     TRANSACTION_RISK_ASSESSMENT = 'transaction_risk_assessment'
+    THREE_DS_OUTAGE = '3ds_outage'
+    SCA_DELEGATION = 'sca_delegation'
+    OUT_OF_SCA_SCOPE = 'out_of_sca_scope'
+    OTHER = 'other'
+    LOW_RISK_PROGRAM = 'low_risk_program'
 
 
 class PaymentDestinationType(str, Enum):
