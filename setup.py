57c3eaecd21ef51e1314aa4bdebe5518093d14a6
Rémy HUBSCHER <hubscher.remy@gmail.com>
2022-07-04 08:53:15 +0200

    Loosen requests version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     license='MIT',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
-        'requests == 2.27.1'
+        'requests >= 2.27.1'
     ],
     test_suite='tests',
     classifiers=[
