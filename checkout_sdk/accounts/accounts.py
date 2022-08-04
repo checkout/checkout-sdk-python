108d536e0a90680a666c6b9be21c2b475430c6f8
Armando Ibarra <aibarrakerzon@gmail.com>
2022-08-04 12:02:13 -0500

    Api updates: Accounts

--- checkout_sdk/accounts/accounts.py
+++ checkout_sdk/accounts/accounts.py
@@ -5,6 +5,46 @@ from checkout_sdk.common.common import ResidentialStatusType, AccountHolderIdent
 from checkout_sdk.common.enums import Currency, InstrumentType, Country, AccountType, AccountHolderType
 
 
+class EntityIdentificationType(str, Enum):
+    PASSPORT = 'passport'
+    NATIONAL_IDENTITY_CARD = 'national_identity_card'
+    DRIVING_LICENSE = 'driving_license'
+    CITIZEN_CARD = 'citizen_card'
+    RESIDENCE_PERMIT = 'residence_permit'
+    ELECTORAL_ID = 'electoral_id'
+
+
+class ScheduleFrequency(str, Enum):
+    WEEKLY = 'weekly'
+    DAILY = 'daily'
+    MONTHLY = 'monthly'
+
+
+class DaySchedule(str, Enum):
+    MONDAY = 'monday'
+    TUESDAY = 'tuesday'
+    WEDNESDAY = 'wednesday'
+    THURSDAY = 'thursday'
+    FRIDAY = 'friday'
+    SATURDAY = 'saturday'
+    SUNDAY = 'sunday'
+
+
+class BusinessType(str, Enum):
+    GENERAL_PARTNERSHIP = 'general_partnership'
+    LIMITED_PARTNERSHIP = 'limited_partnership'
+    PUBLIC_LIMITED_COMPANY = 'public_limited_company'
+    LIMITED_COMPANY = 'limited_company'
+    PROFESSIONAL_ASSOCIATION = 'professional_association'
+    UNINCORPORATED_ASSOCIATION = 'unincorporated_association'
+    AUTO_ENTREPRENEUR = 'auto_entrepreneur'
+
+
+class EntityRoles(str, Enum):
+    UBO = 'ubo'
+    LEGAL_REPRESENTATIVE = 'legal_representative'
+
+
 class ContactDetails:
     phone: Phone
 
@@ -20,15 +60,6 @@ class EntityDocument:
     type: str
 
 
-class EntityIdentificationType(str, Enum):
-    PASSPORT = 'passport'
-    NATIONAL_IDENTITY_CARD = 'national_identity_card'
-    DRIVING_LICENSE = 'driving_license'
-    CITIZEN_CARD = 'citizen_card'
-    RESIDENCE_PERMIT = 'residence_permit'
-    ELECTORAL_ID = 'electoral_id'
-
-
 class EntityIdentificationDocument:
     type: EntityIdentificationType
     front: str
@@ -40,28 +71,50 @@ class EntityIdentification:
     document: EntityIdentificationDocument
 
 
+class DateOfBirth:
+    day: int
+    month: int
+    year: int
+
+
+class PlaceOfBirth:
+    country: Country
+
+
 class EntityRepresentative:
     first_name: str
     middle_name: str
     last_name: str
     address: Address
     identification: EntityIdentification
+    phone: Phone
+    date_of_birth: DateOfBirth
+    place_of_birth: PlaceOfBirth
+    roles: list  # accounts.EntityRoles
+
+
+class EntityFinancialDocuments:
+    bank_statement: EntityDocument
+    financial_statement: EntityDocument
+
+
+class EntityFinancialDetails:
+    annual_processing_volume: int
+    average_transaction_value: int
+    highest_transaction_value: int
+    documents: EntityFinancialDocuments
 
 
 class Company:
     business_registration_number: str
+    business_type: BusinessType
     legal_name: str
     trading_name: str
     principal_address: Address
     registered_address: Address
     document: EntityDocument
     representatives: list  # EntityRepresentative
-
-
-class DateOfBirth:
-    day: int
-    month: int
-    year: int
+    financial_details: EntityFinancialDetails
 
 
 class Identification:
@@ -77,6 +130,7 @@ class Individual:
     national_tax_id: str
     registered_address: Address
     date_of_birth: DateOfBirth
+    place_of_birth: PlaceOfBirth
     identification: Identification
 
 
@@ -101,9 +155,6 @@ class BankDetails:
 
 class AccountsAccountHolder:
     type: AccountHolderType
-    first_name: str
-    last_name: str
-    company_name: str
     tax_id: str
     date_of_birth: DateOfBirth
     country_of_birth: Country
@@ -112,6 +163,15 @@ class AccountsAccountHolder:
     phone: Phone
     identification: AccountHolderIdentification
     email: str
+
+
+class AccountsCorporateAccountHolder(AccountsAccountHolder):
+    company_name: str
+
+
+class AccountsIndividualAccountHolder(AccountsAccountHolder):
+    first_name: str
+    last_name: str
 
 
 class AccountsPaymentInstrument:
@@ -129,22 +189,6 @@ class AccountsPaymentInstrument:
     document: InstrumentDocument
     account_holder: AccountsAccountHolder
     bank: BankDetails
-
-
-class ScheduleFrequency(str, Enum):
-    WEEKLY = 'weekly'
-    DAILY = 'daily'
-    MONTHLY = 'monthly'
-
-
-class DaySchedule(str, Enum):
-    MONDAY = 'monday'
-    TUESDAY = 'tuesday'
-    WEDNESDAY = 'wednesday'
-    THURSDAY = 'thursday'
-    FRIDAY = 'friday'
-    SATURDAY = 'saturday'
-    SUNDAY = 'sunday'
 
 
 class ScheduleRequest:
