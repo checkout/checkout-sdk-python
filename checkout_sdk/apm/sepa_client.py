aeec127500cf7c6e3702d551a770e721c3687834
Martin Seco <mseco.93@gmail.com>
2023-01-25 13:44:49 -0300

    Update Sepa URL for PPro actions

--- checkout_sdk/apm/sepa_client.py
+++ checkout_sdk/apm/sepa_client.py
@@ -7,6 +7,7 @@ from checkout_sdk.client import Client
 
 
 class SepaClient(Client):
+    __APMS_PATH = 'apms'
     __SEPA_MANDATES_PATH = 'sepa/mandates'
     __PPRO_PATH = 'ppro'
     __CANCEL_PATH = 'cancel'
@@ -24,9 +25,11 @@ class SepaClient(Client):
                                      self._sdk_authorization())
 
     def get_mandate_via_ppro(self, mandate_id: str):
-        return self._api_client.get(self.build_path(self.__PPRO_PATH, self.__SEPA_MANDATES_PATH, mandate_id),
+        return self._api_client.get(self.build_path(self.__APMS_PATH, self.__PPRO_PATH,
+                                                    self.__SEPA_MANDATES_PATH, mandate_id),
                                     self._sdk_authorization())
 
     def cancel_mandate_via_ppro(self, mandate_id: str):
-        return self._api_client.post(self.build_path(self.__PPRO_PATH, self.__SEPA_MANDATES_PATH, mandate_id,
-                                                     self.__CANCEL_PATH), self._sdk_authorization())
+        return self._api_client.post(self.build_path(self.__APMS_PATH, self.__PPRO_PATH,
+                                                     self.__SEPA_MANDATES_PATH, mandate_id, self.__CANCEL_PATH),
+                                     self._sdk_authorization())
