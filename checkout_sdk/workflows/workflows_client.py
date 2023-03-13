f72a62a58793ff9ac7420cde638b4aebe60b45c1
Martin Seco <mseco.93@gmail.com>
2023-03-13 10:53:46 -0300

    Api Updates * Adds new sections for adding/deleting workflow actions and workflow conditions (CS2) * Adds Unscheduled payment_type to PaymentRequest (CS1/CS2) * Removes to and from fields from Notifications > Events (CS1)

--- checkout_sdk/workflows/workflows_client.py
+++ checkout_sdk/workflows/workflows_client.py
@@ -23,32 +23,48 @@ class WorkflowsClient(Client):
                          configuration=configuration,
                          authorization_type=AuthorizationType.SECRET_KEY_OR_OAUTH)
 
+    def get_workflows(self):
+        return self._api_client.get(self.__WORKFLOWS_PATH,
+                                    self._sdk_authorization())
+
     def create_workflow(self, create_workflow_request: CreateWorkflowRequest):
         return self._api_client.post(self.__WORKFLOWS_PATH,
                                      self._sdk_authorization(),
                                      create_workflow_request)
 
-    def get_workflows(self):
-        return self._api_client.get(self.__WORKFLOWS_PATH,
-                                    self._sdk_authorization())
-
     def get_workflow(self, workflow_id: str):
         return self._api_client.get(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
                                     self._sdk_authorization())
+
+    def remove_workflow(self, workflow_id: str):
+        return self._api_client.delete(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
+                                       self._sdk_authorization())
 
     def update_workflow(self, workflow_id: str, update_workflow_request: UpdateWorkflowRequest):
         return self._api_client.patch(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
                                       self._sdk_authorization(),
                                       update_workflow_request)
 
-    def remove_workflow(self, workflow_id: str):
-        return self._api_client.delete(self.build_path(self.__WORKFLOWS_PATH, workflow_id),
-                                       self._sdk_authorization())
+    def add_workflow_action(self, workflow_id: str, workflow_action_request: WorkflowActionRequest):
+        return self._api_client.post(self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__ACTIONS_PATH),
+                                     self._sdk_authorization(),
+                                     workflow_action_request)
 
     def update_workflow_action(self, workflow_id: str, action_id: str, workflow_action_request: WorkflowActionRequest):
         return self._api_client.put(self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__ACTIONS_PATH, action_id),
                                     self._sdk_authorization(),
                                     workflow_action_request)
+
+    def remove_workflow_action(self, workflow_id: str, action_id: str):
+        return self._api_client.delete(
+            self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__ACTIONS_PATH, action_id),
+            self._sdk_authorization())
+
+    def add_workflow_condition(self, workflow_id: str, workflow_condition_request: WorkflowConditionRequest):
+        return self._api_client.post(
+            self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__CONDITIONS_PATH),
+            self._sdk_authorization(),
+            workflow_condition_request)
 
     def update_workflow_condition(self, workflow_id: str, condition_id: str,
                                   workflow_condition_request: WorkflowConditionRequest):
@@ -57,29 +73,28 @@ class WorkflowsClient(Client):
             self._sdk_authorization(),
             workflow_condition_request)
 
+    def remove_workflow_condition(self, workflow_id: str, condition_id: str):
+        return self._api_client.delete(
+            self.build_path(self.__WORKFLOWS_PATH, workflow_id, self.__CONDITIONS_PATH, condition_id),
+            self._sdk_authorization())
+
     def get_event_types(self):
         return self._api_client.get(self.build_path(self.__WORKFLOWS_PATH, self.__EVENT_TYPES_PATH),
                                     self._sdk_authorization())
-
-    def get_subject_events(self, subject_id: str):
-        return self._api_client.get(
-            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id),
-            self._sdk_authorization())
 
     def get_event(self, event_id: str):
         return self._api_client.get(
             self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id),
             self._sdk_authorization())
 
+    def get_action_invocations(self, event_id: str, action_id: str):
+        return self._api_client.get(
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__ACTIONS_PATH, action_id),
+            self._sdk_authorization())
+
     def reflow_by_event(self, event_id: str):
         return self._api_client.post(
             self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__REFLOW_PATH),
-            self._sdk_authorization())
-
-    def reflow_by_subject(self, subject_id: str):
-        return self._api_client.post(
-            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
-                            self.__REFLOW_PATH),
             self._sdk_authorization())
 
     def reflow_by_event_and_workflow(self, event_id: str, workflow_id: str):
@@ -88,18 +103,24 @@ class WorkflowsClient(Client):
                             self.__REFLOW_PATH),
             self._sdk_authorization())
 
-    def reflow_by_subject_and_workflow(self, subject_id: str, workflow_id: str):
-        return self._api_client.post(
-            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
-                            self.__WORKFLOW_PATH, workflow_id, self.__REFLOW_PATH),
-            self._sdk_authorization())
-
     def reflow(self, reflow_request: ReflowRequest):
         return self._api_client.post(self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__REFLOW_PATH),
                                      self._sdk_authorization(),
                                      reflow_request)
 
-    def get_action_invocations(self, event_id: str, action_id: str):
+    def get_subject_events(self, subject_id: str):
         return self._api_client.get(
-            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, event_id, self.__ACTIONS_PATH, action_id),
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id),
+            self._sdk_authorization())
+
+    def reflow_by_subject(self, subject_id: str):
+        return self._api_client.post(
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
+                            self.__REFLOW_PATH),
+            self._sdk_authorization())
+
+    def reflow_by_subject_and_workflow(self, subject_id: str, workflow_id: str):
+        return self._api_client.post(
+            self.build_path(self.__WORKFLOWS_PATH, self.__EVENTS_PATH, self.__SUBJECT_PATH, subject_id,
+                            self.__WORKFLOW_PATH, workflow_id, self.__REFLOW_PATH),
             self._sdk_authorization())
--- tests/workflows/workflows_client_test.py
+++ tests/workflows/workflows_client_test.py
@@ -12,67 +12,83 @@ def client(mock_sdk_configuration, mock_api_client):
 
 class TestWorkflowsClient:
 
-    def test_create_workflow(self, mocker, client: WorkflowsClient):
-        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
-        assert client.create_workflow(CreateWorkflowRequest()) == 'response'
-
     def test_get_workflows(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
         assert client.get_workflows() == 'response'
+
+    def test_create_workflow(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
+        assert client.create_workflow(CreateWorkflowRequest()) == 'response'
 
     def test_get_workflow(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
         assert client.get_workflow('workflow_id') == 'response'
 
-    def test_update_workflow(self, mocker, client: WorkflowsClient):
-        mocker.patch('checkout_sdk.api_client.ApiClient.patch', return_value='response')
-        assert client.update_workflow('workflow_id', UpdateWorkflowRequest()) == 'response'
-
     def test_remove_workflow(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.delete', return_value='response')
         assert client.remove_workflow('workflow_id') == 'response'
 
+    def test_update_workflow(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.patch', return_value='response')
+        assert client.update_workflow('workflow_id', UpdateWorkflowRequest()) == 'response'
+
+    def test_add_workflow_action(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
+        assert client.add_workflow_action('workflow_id', WebhookWorkflowActionRequest()) == 'response'
+
     def test_update_workflow_action(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.put', return_value='response')
         assert client.update_workflow_action('workflow_id', 'action_id', WebhookWorkflowActionRequest()) == 'response'
+
+    def test_remove_workflow_action(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.delete', return_value='response')
+        assert client.remove_workflow_action('workflow_id', 'action_id') == 'response'
+
+    def test_add_workflow_condition(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
+        assert client.add_workflow_condition('workflow_id', EventWorkflowConditionRequest()) == 'response'
 
     def test_update_workflow_condition(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.put', return_value='response')
         assert client.update_workflow_condition('workflow_id', 'condition_id',
                                                 EventWorkflowConditionRequest()) == 'response'
 
+    def test_remove_workflow_condition(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.delete', return_value='response')
+        assert client.remove_workflow_condition('workflow_id', 'condition_id') == 'response'
+
     def test_get_event_types(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
         assert client.get_event_types() == 'response'
-
-    def test_get_subject_events(self, mocker, client: WorkflowsClient):
-        mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
-        assert client.get_subject_events('subject_id') == 'response'
 
     def test_get_event(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
         assert client.get_event('event_id') == 'response'
 
+    def test_should_get_action_invocations(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
+        assert client.get_action_invocations('action_id', 'event_id') == 'response'
+
     def test_reflow_by_event(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
         assert client.reflow_by_event('event_id') == 'response'
-
-    def test_reflow_by_subject(self, mocker, client: WorkflowsClient):
-        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
-        assert client.reflow_by_subject('subject_id') == 'response'
 
     def test_reflow_by_event_and_workflow(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
         assert client.reflow_by_event_and_workflow('event_id', 'workflow_id') == 'response'
 
-    def test_reflow_by_subject_and_workflow(self, mocker, client: WorkflowsClient):
-        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
-        assert client.reflow_by_subject_and_workflow('subject_id', 'workflow_id') == 'response'
-
     def test_reflow(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
         assert client.reflow(ReflowRequest()) == 'response'
 
-    def test_should_get_action_invocations(self, mocker, client: WorkflowsClient):
+    def test_get_subject_events(self, mocker, client: WorkflowsClient):
         mocker.patch('checkout_sdk.api_client.ApiClient.get', return_value='response')
-        assert client.get_action_invocations('action_id', 'event_id') == 'response'
+        assert client.get_subject_events('subject_id') == 'response'
+
+    def test_reflow_by_subject(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
+        assert client.reflow_by_subject('subject_id') == 'response'
+
+    def test_reflow_by_subject_and_workflow(self, mocker, client: WorkflowsClient):
+        mocker.patch('checkout_sdk.api_client.ApiClient.post', return_value='response')
+        assert client.reflow_by_subject_and_workflow('subject_id', 'workflow_id') == 'response'
