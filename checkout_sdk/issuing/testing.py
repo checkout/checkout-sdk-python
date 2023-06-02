d2d1422ff15f3f965597ae32c8c28f1eb97d2a5c
Martin Seco <mseco.93@gmail.com>
2023-06-02 13:14:27 -0300

    Adds support for Testing operations [Issuing] (CS2) * Adds support for `simulate_authorization`

---
+++ checkout_sdk/issuing/testing.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+from checkout_sdk.common.enums import Currency
+
+
+class TransactionType(str, Enum):
+    PURCHASE = 'purchase'
+
+
+class AuthorizationType(str, Enum):
+    AUTHORIZATION = 'authorization'
+    PRE_AUTHORIZATION = 'pre_authorization'
+
+
+class CardSimulation:
+    id: str
+    expiry_month: int
+    expiry_year: int
+
+
+class Merchant:
+    category_code: str
+
+
+class TransactionSimulation:
+    type: TransactionType
+    amount: int
+    currency: Currency
+    merchant: Merchant
+    authorization_type: AuthorizationType
+
+
+class CardAuthorizationRequest:
+    card: CardSimulation
+    transaction: TransactionSimulation
