ce92fd1519d4cd8877e01724454a0c2b0fd76cdb
Mate Zoltan <mate.zoltan0@gmail.com>
2023-12-07 10:31:53 +0100

    Add serializer to response wrapper
    It can be useful, when the response fully or partially need to be stored
    in a database in a JSON serialized form.
    
    From python 3.8, the `_unwrap` method could be replaced with
    using singledispatchmethod.

--- checkout_sdk/checkout_response.py
+++ checkout_sdk/checkout_response.py
@@ -1,3 +1,7 @@
+from collections.abc import Iterable, Mapping
+from inspect import isfunction, ismethod
+
+
 class ResponseWrapper:
 
     def __init__(self, http_metadata=None, data=None):
@@ -21,3 +25,77 @@ class ResponseWrapper:
     @staticmethod
     def _is_collection(value):
         return isinstance(value, (tuple, list, set, frozenset))
+
+    def dict(self):
+        """
+        Serializes the instance to a dictionary recursively.
+
+        The result shall only contain JSON compatible data structures.
+
+        Circular references shall be replaced with representations of appropriate
+        JSON references (https://json-spec.readthedocs.io/reference.html).
+        """
+        return self._unwrap_object(self, paths_by_id={}, path=['#'])
+
+    @staticmethod
+    def _handle_circular_ref(method):
+        def decorated_method(cls, data, paths_by_id, path):
+            if id(data) in paths_by_id:
+                for ref_path in paths_by_id[id(data)]:
+                    if path[:len(ref_path)] == ref_path:
+                        return {'$ref': '/'.join(ref_path)}
+
+                paths_by_id[id(data)].append(path)
+
+            else:
+                paths_by_id[id(data)] = [path]
+
+            return method(cls, data, paths_by_id, path)
+
+        return decorated_method
+
+    @classmethod
+    @_handle_circular_ref
+    def _unwrap_object(cls, data, paths_by_id, path):
+        return {
+            key: cls._unwrap(attr, paths_by_id, path + [key])
+            for key in dir(data)
+            if not key.startswith('__')
+            and not cls._is_function(attr := getattr(data, key))
+        }
+
+    @classmethod
+    def _unwrap(cls, data, paths_by_id, path):
+        if isinstance(data, (str, int, float, bool, type(None))):
+            return data
+
+        elif isinstance(data, Mapping):
+            return cls._unwrap_mapping(data, paths_by_id, path)
+
+        elif isinstance(data, Iterable):
+            return cls._unwrap_iterable(data, paths_by_id, path)
+
+        else:
+            return cls._unwrap_object(data, paths_by_id, path)
+
+    @classmethod
+    @_handle_circular_ref
+    def _unwrap_mapping(cls, data: Mapping, paths_by_id, path):
+        return {
+            key: cls._unwrap(value, paths_by_id, path + [key])
+            for key, value in data.items()
+            if not cls._is_function(value)
+        }
+
+    @classmethod
+    @_handle_circular_ref
+    def _unwrap_iterable(cls, data: Iterable, paths_by_id, path):
+        return [
+            cls._unwrap(value, paths_by_id, path + [str(idx)])
+            for idx, value in enumerate(data)
+            if not cls._is_function(value)
+        ]
+
+    @staticmethod
+    def _is_function(data):
+        return isfunction(data) or ismethod(data)
